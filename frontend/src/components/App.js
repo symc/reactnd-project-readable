892a5cc41c0b4c2b2c2971e8371311974e5a15d8
symc <sina.yamac@gmail.com>
2017-11-11 16:34:54 -0500

    chore: Install Bootstrap
    Installed Bootstrap

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,9 +4,9 @@ import '../styles/App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello World!
-      </div>
+        <div className="App">
+            <button type="button" className="btn btn-primary">Hello world</button>
+        </div>
     );
   }
 }
