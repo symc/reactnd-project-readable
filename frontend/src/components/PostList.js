e1bfffad5a457955e75fae7c76c9f632812fcaea
symc <sina.yamac@gmail.com>
2017-12-03 14:19:02 -0500

    feat: Sorting and category filters are functional
    The user can sort the posts by votes (higher to lower), date of
    creating (recent to late) and title (alphabetical) using the sort
    options in the navigation bar. The user can filter posts by category
    using the blue buttons in the categories bar.

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -25,9 +25,46 @@ function mapDispatchToProps(dispatch) {
     return {};
 }
 
-function mapStateToProps({categories, posts, comments}) {
+function sortPosts(posts, postIds, sortMethod) {
+    let tuples = postIds.map((id) => {
+        switch (sortMethod) {
+            case 'votes':
+                // Sort by decreasing scores
+                // Higher score posts will appear earlier than lower scores
+                return [-posts[id].voteScore, id];
+            case 'dates':
+                // Sort by decreasing time stamp
+                // Recent posts will appear earlier than later posts
+                return [-posts[id].timestamp, id]
+            case 'titles':
+                // Alphabetical sorting of the post titles
+                return [posts[id].title, id];
+            default:
+                return [id, id];
+        }
+    });
+    tuples.sort();
+    return tuples.map((tuple) => {
+        return tuple[1];
+    });
+}
+
+function filterAndSortPosts(posts, selectedCategory, sortMethod) {
+    if (!selectedCategory) {
+        return sortPosts(posts, Object.keys(posts), sortMethod);
+    }
+    const answer = Object.keys(posts).filter((id) => {
+        return (posts[id].category === selectedCategory);
+    });
+    return sortPosts(posts, answer, sortMethod);
+}
+
+const mapStateToProps = (state, ownProps) => {
+    const selectedCategory = ownProps.match.params.category;
+    const sortMethod = state.listState.sortBy;
+    const postIds = filterAndSortPosts(state.posts, selectedCategory, sortMethod);
     return {
-        postIds: Object.keys(posts)
+        postIds: postIds
     };
 }
 
